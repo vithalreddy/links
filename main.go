9fa8a3830fcc69e18b804c99daabdd3a1ad128c6
Vithal Reddy <vmreddyvmb@gmail.com>
2020-06-14 19:47:41 +0530

    added heartBeatNestBrahma fn

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ var links = map[string]string{}
 
 func main() {
 	go heartBeat()
+	go heartBeatNestBrahma()
 
 	port := "8090"
 	if value, ok := os.LookupEnv("PORT"); ok {
@@ -63,6 +64,24 @@ func heartBeat() {
 				log.Fatal(err)
 			} else {
 				color.HiGreen(":: HeartBeat ::success:: -> " + string(data))
+			}
+		}
+
+	}
+}
+
+
+func heartBeatNestBrahma() {
+	for range time.Tick(time.Second * 30) {
+		res, err := http.Get("https://nest-brahma.herokuapp.com")
+		if err != nil {
+			color.HiRed(":: HeartBeatNestBrahma ::error::  " + err.Error())
+		} else {
+			data, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				color.HiGreen(":: HeartBeatNestBrahma ::success:: -> " + string(data))
 			}
 		}
 
